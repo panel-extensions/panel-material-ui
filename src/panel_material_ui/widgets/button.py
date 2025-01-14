5598ecc6a8b4b90812b181cf64d18d37a35c790d
Philipp Rudiger <prudiger@anaconda.com>
2025-01-14 18:58:46 +0100

    Button and Icon cleanup

--- src/panel_material_ui/widgets/button.py
+++ src/panel_material_ui/widgets/button.py
@@ -67,11 +67,6 @@ class Button(_ButtonBase, _ClickButton):
     It also provides an additional `clicks` parameter, that can be
     watched to subscribe to click events.
 
-    Some missing and extra features (if any) when comparing with the corresponding
-    panel Button widget [panel.widgets.Button](https://panel.holoviz.org/reference/widgets/Button.html):
-    - Missing features: description_delay
-    - Extra features: label, on_event, on_msg, tooltip (work in progress), theme
-
     :Example:
 
     >>> Button(name='Click me', icon='caret-right', button_type='primary')
@@ -133,12 +128,6 @@ class Toggle(_ButtonBase):
 
     This widget is interchangeable with the `Checkbox` widget.
 
-    Some missing and extra features (if any) when comparing with the corresponding
-    panel Toggle widget [panel.widgets.Toggle](https://panel.holoviz.org/reference/widgets/Toggle.html):
-    - No missing features
-    - Extra features: clicks, description, label, on_event, on_msg, theme
-
-
     :Example:
 
     >>> Toggle(name='Toggle', button_type='success')
@@ -153,59 +142,3 @@ class Toggle(_ButtonBase):
     value = param.Boolean(default=False)
 
     _esm = "ToggleButton.jsx"
-
-
-class ButtonIcon(_ButtonBase):
-    """
-    The `ButtonIcon` widget facilitates event triggering upon button clicks.
-
-    This widget displays a default `icon` initially. Upon being clicked, an `active_icon` appears
-    for a specified `toggle_duration`.
-
-    For instance, the `ButtonIcon` can be effectively utilized to implement a feature akin to
-    ChatGPT's copy-to-clipboard button.
-
-    The button incorporates a `value` attribute, which alternates between `False` and `True` as the
-    click event is processed.
-
-    Furthermore, it includes an `clicks` attribute, enabling subscription to click events for
-    further actions or monitoring.
-
-    Some missing and extra features (if any) when comparing with the corresponding
-    panel ButtonIcon widget [panel.widgets.ButtonIcon](https://panel.holoviz.org/reference/widgets/ButtonIcon.html):
-    - Missing features: description_delay, js_on_click, on_click
-    - Extra features: button_style, button_type, edge, label, on_event, on_msg, theme
-
-    :Example:
-
-    >>> button_icon = ButtonIcon(
-    ...     icon='favorite',
-    ...     active_icon='check',
-    ...     description='Copy',
-    ...     toggle_duration=2000
-    ... )
-    """
-
-    active_icon = param.String(
-        default="",
-        doc="""
-        The name of the icon to display when toggled from
-        [tabler-icons.io](https://tabler-icons.io)/ or an SVG.""",
-    )
-
-    edge = param.Selector(objects=["start", "end", False], default=False)
-
-    size = param.String(
-        default="1em",
-        doc="""
-        Size of the icon as a string, e.g. 12px or 1em.""",
-    )
-
-    toggle_duration = param.Integer(
-        default=75,
-        doc="""
-        The number of milliseconds the active_icon should be shown for
-        and how long the button should be disabled for.""",
-    )
-
-    _esm = "IconButton.jsx"
