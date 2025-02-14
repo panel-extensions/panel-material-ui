a6b4a552d0d1f181a3cf82082cf4bcdab5ac8973
Philipp Rudiger <prudiger@anaconda.com>
2025-02-14 16:50:15 +0100

    Fix versioning and CDN loading code (#65)

--- src/panel_material_ui/__version.py
+++ src/panel_material_ui/__version.py
@@ -3,21 +3,23 @@
 Called __version.py as setuptools_scm will create a _version.py
 """
 
-import os.path
+import pathlib
 
 PACKAGE = "panel_material_ui"
 
 try:
     # For performance reasons on imports, avoid importing setuptools_scm
     # if not in a .git folder
-    if os.path.exists(os.path.join(os.path.dirname(__file__), "..", ".git")):
+    if (pathlib.Path(__file__).parent.parent.parent / ".git").is_dir():
         # If setuptools_scm is installed (e.g. in a development environment with
         # an editable install), then use it to determine the version dynamically.
         from setuptools_scm import get_version
 
         # This will fail with LookupError if the package is not installed in
         # editable mode or if Git is not installed.
-        __version__ = get_version(root="..", relative_to=__file__, version_scheme="post-release")
+        __version__ = get_version(
+            root="../..", relative_to=__file__, version_scheme="post-release"
+        )
     else:
         raise FileNotFoundError
 except (ImportError, LookupError, FileNotFoundError):
@@ -25,7 +27,7 @@ except (ImportError, LookupError, FileNotFoundError):
     try:
         # __version__ was added in _version in setuptools-scm 7.0.0, we rely on
         # the hopefully stable version variable.
-        from ._version import version as __version__
+        from ._version import version as __version__  # type: ignore
     except (ModuleNotFoundError, ImportError):
         # Either _version doesn't exist (ModuleNotFoundError) or version isn't
         # in _version (ImportError). ModuleNotFoundError is a subclass of
