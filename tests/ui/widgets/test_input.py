1ac07bd9a41f8b5b40f9a59a3c3847c34c4a978b
thuydotm <thuydtm.csk9@gmail.com>
2025-03-10 17:24:06 +0700

    test disabled/enabled dates

--- tests/ui/widgets/test_input.py
+++ tests/ui/widgets/test_input.py
@@ -2,8 +2,13 @@ import pytest
 
 pytest.importorskip('playwright')
 
+import datetime as dt
+
 from panel.tests.util import serve_component, wait_until
-from panel_material_ui.widgets import TextInput, PasswordInput, TextAreaInput, Checkbox, Switch, FileInput, ToggleIcon
+from panel_material_ui.widgets import (
+    DatePicker,
+    TextInput, PasswordInput, TextAreaInput, Checkbox, Switch, FileInput, ToggleIcon,
+)
 from playwright.sync_api import expect
 
 pytestmark = pytest.mark.ui
@@ -252,3 +257,64 @@ def test_toggle_icon(page):
     expect(icon).to_have_text("thumb-up")
     icon.click()
     expect(icon).to_have_text("thumb-down")
+
+
+def test_datepicker_enabled_dates(page):
+    widget = DatePicker(
+        name='Date Picker',
+        start=dt.date(2024, 4, 1),
+        end=dt.date(2024, 4, 20),
+        enabled_dates=[
+            dt.date(2024, 4, 1),
+            dt.date(2024, 4, 2),
+            dt.date(2024, 4, 20),
+            dt.date(2024, 4, 21),
+        ]
+    )
+    serve_component(page, widget)
+
+    # click the datepicker icon to show dates to select
+    icon = page.locator(".MuiIconButton-root")
+    icon.click()
+    # Select all buttons in the date picker
+    buttons = page.locator('.MuiPickersDay-root').all()
+
+    enabled_buttons = []
+    for button in buttons:
+        is_disabled = button.get_attribute("disabled") is not None or button.evaluate(
+            "el => el.classList.contains('Mui-disabled')")
+        if not is_disabled:
+            value = button.inner_text()  # Get the button's displayed text (date)
+            enabled_buttons.append(value)
+
+    # only enabled dates within the start to end range are selectable
+    assert enabled_buttons == ['1', '2', '20']
+
+
+def test_datepicker_disabled_dates(page):
+    widget = DatePicker(
+        name='Date Picker',
+        start=dt.date(2024, 4, 1),
+        end=dt.date(2024, 4, 20),
+        disabled_dates=[
+            dt.date(2024, 4, i) for i in range(1, 18)
+        ]
+    )
+    serve_component(page, widget)
+
+    # click the datepicker icon to show dates to select
+    icon = page.locator(".MuiIconButton-root")
+    icon.click()
+    # Select all buttons in the date picker
+    buttons = page.locator('.MuiPickersDay-root').all()
+
+    enabled_buttons = []
+    for button in buttons:
+        is_disabled = button.get_attribute("disabled") is not None or button.evaluate(
+            "el => el.classList.contains('Mui-disabled')")
+        if not is_disabled:
+            value = button.inner_text()  # Get the button's displayed text (date)
+            enabled_buttons.append(value)
+
+    # only enabled dates within the start to end range are selectable
+    assert enabled_buttons == ['18', '19', '20']
