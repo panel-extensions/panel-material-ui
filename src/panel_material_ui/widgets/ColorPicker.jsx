b80b44d4a900b645323cb768b111c3848f91a1f1
Philipp Rudiger <prudiger@anaconda.com>
2025-03-13 22:01:37 +0100

    Fix indent

--- src/panel_material_ui/widgets/ColorPicker.jsx
+++ src/panel_material_ui/widgets/ColorPicker.jsx
@@ -1,23 +1,24 @@
 import { MuiColorInput } from 'mui-color-input'
 
 export function render({model, el}) {
-  const [value, setValue] = model.useState("value")
   const [alpha] = model.useState("alpha")
-  const [format] = model.useState("format")
-  const [variant] = model.useState("variant")
-  const [size] = model.useState("size")
-  const [disabled] = model.useState("disabled")
   const [color] = model.useState("color")
+  const [disabled] = model.useState("disabled")
+  const [format] = model.useState("format")
+  const [size] = model.useState("size")
+  const [variant] = model.useState("variant")
+  const [value, setValue] = model.useState("value")
+
   return (
     <MuiColorInput
-      disabled={disabled}
       color={color}
+      disabled={disabled}
       format={format}
-      value={value}
-      onChange={setValue}
       isAlphaHidden={!alpha}
-      variant={variant}
+      onChange={setValue}
       size={size}
+      value={value}
+      variant={variant}
       PopoverProps={{
         container: el,
       }}
