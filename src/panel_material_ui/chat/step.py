37a5d929b85c15a7a07dfc1c34126be273844f54
Philipp Rudiger <prudiger@anaconda.com>
2025-03-20 11:20:44 +0100

    Updates for ChatStep and ChatAreaInput

--- src/panel_material_ui/chat/step.py
+++ src/panel_material_ui/chat/step.py
@@ -1,10 +1,36 @@
+from __future__ import annotations
+
+import param
+from panel._param import Margin
+from panel.chat.step import ChatStep as _PnChatStep
 
 from ..layout import Card
 
 
-class ChatStep(Card):
+class ChatStep(Card, _PnChatStep):
     """
+    A component that makes it easy to provide status updates and the
+    ability to stream updates to both the output(s) and the title.
+
+    Reference: https://panel.holoviz.org/reference/chat/ChatStep.html
+
+    :Example:
+
+    >>> ChatStep("Hello world!", title="Running calculation...', status="running")
     """
 
-    _rename = {"objects": "objects"}
+    margin = Margin(default=0)
+
+    _esm_base = "ChatStep.jsx"
+    _rename = {
+        "objects": "objects", "header": None, "title": "title", "status": "status"}
     _stylesheets = []
+
+    def __init__(self, *objects, **params):
+        self._instance = None
+        self._failed_title = ""
+        Card.__init__(self, *objects, **params)
+
+    @param.depends("status", "default_badges", watch=True)
+    def _render_avatar(self):
+        return
