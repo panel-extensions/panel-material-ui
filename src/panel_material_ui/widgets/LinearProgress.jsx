fba054c421b25a3a278ceb1324a10e285ddf3de5
Philipp Rudiger <prudiger@anaconda.com>
2025-04-14 15:47:21 +0200

    Clean up and improve Indicators

--- src/panel_material_ui/widgets/LinearProgress.jsx
+++ src/panel_material_ui/widgets/LinearProgress.jsx
@@ -1,11 +1,13 @@
 import LinearProgress from "@mui/material/LinearProgress";
 
 export function render({model}) {
-  const [value] = model.useState("value");
-  const [variant] = model.useState("variant");
-  const [color] = model.useState("color");
-  const [sx] = model.useState("sx");
+  const [color] = model.useState("color")
+  const [sx] = model.useState("sx")
+  const [value] = model.useState("value")
+  const [variant] = model.useState("variant")
+  const [valueBuffer] = model.useState("value_buffer")
+
   return (
-    <LinearProgress color={color} variant={variant} value={value} sx={sx} />
-  );
+    <LinearProgress color={color} variant={variant} value={value} valueBuffer={valueBuffer} sx={sx} />
+  )
 }
