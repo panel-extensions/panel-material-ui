ca002de36a63f48105f6e3182c239aaa623f475e
Philipp Rudiger <prudiger@anaconda.com>
2025-04-30 00:50:43 +0200

    Toggle dark theme from query parameter (#211)

--- src/panel_material_ui/utils.js
+++ src/panel_material_ui/utils.js
@@ -553,7 +553,8 @@ export const install_theme_hooks = (props) => {
   React.useEffect(() => {
     // If the page has a data-theme attribute (e.g. from pydata-sphinx-theme), use it to set the dark theme
     const page_theme = document.documentElement.dataset.theme
-    if (page_theme === "dark") {
+    const params = new URLSearchParams(window.location.search);
+    if (page_theme === "dark" || params.get('theme') === 'dark') {
       setDarkTheme(true)
     } else if (page_theme === "light") {
       setDarkTheme(false)
