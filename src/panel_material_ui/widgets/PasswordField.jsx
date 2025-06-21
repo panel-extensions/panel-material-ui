14ccdf6f73fc6b723969192081379815b7eec4db
Philipp Rudiger <prudiger@anaconda.com>
2025-06-21 14:38:33 +0200

    Align TextInput, PasswordInput and AutocompleteInput (#322)

--- src/panel_material_ui/widgets/PasswordField.jsx
+++ src/panel_material_ui/widgets/PasswordField.jsx
@@ -7,18 +7,35 @@ import VisibilityOff from "@mui/icons-material/VisibilityOff"
 export function render({model}) {
   const [color] = model.useState("color")
   const [disabled] = model.useState("disabled")
+  const [error_state] = model.useState("error_state")
   const [label] = model.useState("label")
   const [max_length] = model.useState("max_length")
   const [placeholder] = model.useState("placeholder")
-  const [value, setValue] = model.useState("value")
-  const [variant] = model.useState("variant")
+  const [size] = model.useState("size")
   const [sx] = model.useState("sx")
+  const [value, setValue] = model.useState("value")
+  const [value_input, setValueInput] = model.useState("value_input")
+  const [variant] = model.useState("variant")
   const [showPassword, setShowPassword] = React.useState(false)
 
   return (
     <TextField
       color={color}
       disabled={disabled}
+      error={error_state}
+      fullWidth
+      inputProps={{maxLength: max_length}}
+      label={label}
+      onBlur={() => setValue(value_input)}
+      onChange={(event) => setValueInput(event.target.value)}
+      onKeyDown={(event) => {
+        if (event.key === "Enter") {
+          model.send_event("enter", event)
+          setValue(value_input)
+        }
+      }}
+      maxLength={max_length}
+      size={size}
       slotProps={{
         input: {
           endAdornment: (
@@ -37,15 +54,10 @@ export function render({model}) {
           )
         }
       }}
-      fullWidth
-      inputProps={{maxLength: max_length}}
-      label={label}
-      onChange={(event) => setValue(event.target.value)}
-      placeholder={placeholder}
       sx={sx}
       type={showPassword ? "text" : "password"}
       variant={variant}
-      value={value}
+      value={value_input}
     />
   )
 }
