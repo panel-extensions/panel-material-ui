ef6c55d87427bb00671a3fba7207db22d56cfc2a
Philipp Rudiger <prudiger@anaconda.com>
2025-12-08 16:27:43 +0100

    Yet another test adjustment

--- tests/ui/widgets/test_editable_range_slider.py
+++ tests/ui/widgets/test_editable_range_slider.py
@@ -216,7 +216,7 @@ def test_editable_range_slider_increment_decrement_buttons(page):
 
 @pytest.mark.parametrize("inline_layout,targets", [
     (False, [87, 200, 240]),
-    (True, [56, 131, 157])
+    (True, [78, 181, 216])
 ])
 def test_editable_range_slider_slider_interaction(page, inline_layout, targets):
     x1, x2, x3 = targets
@@ -227,7 +227,7 @@ def test_editable_range_slider_slider_interaction(page, inline_layout, targets):
         value=(2, 8),
         step=0.1,
         inline_layout=inline_layout,
-        width=400 if inline_layout else 300
+        width=500 if inline_layout else 300
     )
     serve_component(page, widget)
 
