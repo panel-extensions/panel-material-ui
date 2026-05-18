9f57f0e7c297d4baf12427fb2e9195dfee0bf8e9
Philipp Rudiger <prudiger@anaconda.com>
2026-05-18 20:51:45 +0200

    Do not override Vizzu style if provided

--- src/panel_material_ui/utils.js
+++ src/panel_material_ui/utils.js
@@ -794,7 +794,7 @@ function apply_bokeh_theme(model, theme, dark, font_family, custom_theme=[]) {
         --mdc-theme-surface: ${elevation_color(elevation+1, theme, dark, true)};
       }
     `]
-  } else if (model_type.endsWith("VizzuChart")) {
+  } else if (model_type.endsWith("VizzuChart") && (model.style == null || Object.keys(model.style).length === 0)) {
     const view = Bokeh.index.find_one_by_id(model.id)
     const elevation = view ? find_on_parent(view, "elevation") : 0
     const background = elevation_color(elevation, theme, dark, true)
